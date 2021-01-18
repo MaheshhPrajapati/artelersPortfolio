54cd18041eceb334dd2b181a361f453c8946a3ef
MaheshhPrajapati <59406642+MaheshhPrajapati@users.noreply.github.com>
2021-01-18 19:25:11 +0530

    added 'background change on scroll from white to yellow

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ new kursor({
     type: 5,
     removeDefaultCursor: true
 });
+
+window.addEventListener('scroll', (e) => {
+    if(window.scrollY > window.innerHeight / 3){
+        document.body.classList.add('bg-color');
+    }else{
+        document.body.classList.remove('bg-color');
+    }
+})
