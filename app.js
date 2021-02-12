9aedb8ad06eb44c40a1d835998082cf902781cfd
MaheshhPrajapati <59406642+MaheshhPrajapati@users.noreply.github.com>
2021-02-12 15:56:25 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-AOS.init();
 
-new kursor({
-    type: 5,
-    removeDefaultCursor: true
-});
-
-window.addEventListener('scroll', (e) => {
-    if(window.scrollY > window.innerHeight / 3){
-        document.body.classList.add('bg-color');
-    }else{
-        document.body.classList.remove('bg-color');
-    }
-})
+function toggle(){
+    document.getElementById("nav-links").classList.toggle("active");
+}
